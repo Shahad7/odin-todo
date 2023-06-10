b207c7d1bce0d67ab0f418fb0f50343662a0391e
shahad7 <shahadneeliyatt786@gmail.com>
2023-06-10 19:16:23 +0530

    finish saving projects functionality

---
+++ src/index.js
@@ -0,0 +1,158 @@
+import './style.css'
+import closeICon from './close.svg'
+import pencilIcon from './pencil.svg'
+import eyeIcon from './eye.svg'
+import plusICon from './plus.svg'
+import trashIcon from './trash.svg'
+import tickIcon from './tick.svg';
+import projectIcon from './project.svg'
+import inboxIcon from './inbox.svg'
+import todayIcon from './today.svg'
+import weekIcon from './week.svg'
+import flagIcon from './flag.svg';
+
+//start
+
+const taskConstructor = (title,description,dueDate,priority) => {
+    return {title,description,dueDate,priority}
+
+}
+
+const projectHandler = (function (){
+
+    let projects = [];
+    const projectConstructor = (title) => {
+        return {"title":title,"tasks":[]}
+    }
+
+    const pushProject = (obj)=>{
+        projects.push(obj)
+    }
+
+    const getProjects = ()=>{
+        return projects;
+    }
+
+    const isDuplicate = (str)=>{
+        let found = 0
+        for(let i in projects){
+            if(projects[i].title==str)
+            {
+                found = 1
+                return true
+            }         
+        }
+        if(found==0)
+        {
+            return false
+        }
+
+    }
+
+    return{projectConstructor,pushProject,getProjects,isDuplicate}
+
+})();
+
+
+const DOMHandler = (function (){
+
+    //event listeners
+
+    const range = document.getElementById('priority')
+    range.value = '1';
+    range.addEventListener('input',()=>{
+        changeColor()
+    })
+
+    function changeColor(){
+        if(range.value=='0')
+            range.style.accentColor = 'green'
+        else if(range.value=='1')
+            range.style.accentColor = 'yellow'
+        else if(range.value=='2')
+            range.style.accentColor = 'red'
+    }
+
+
+    //closing n opening task form
+    const formCloseButton = document.querySelector('#form-close-button')
+    const newTaskButton = document.querySelector('.add-task')
+    const newTaskForm = document.querySelector('.add-task-form')
+    const content = document.querySelector('.content')
+
+    formCloseButton.addEventListener('click',()=>{
+        newTaskForm.style.visibility = 'hidden'
+        newTaskForm.style.transform = 'scale(0.2)'
+        content.style.filter = 'none'
+    })
+
+    newTaskButton.addEventListener('click',()=>{
+        newTaskForm.style.visibility = 'visible'
+        newTaskForm.style.transform = 'scale(1)'
+        content.style.filter = 'blur(8px)'
+    })
+
+
+    //new project opening n closing
+    const newProjectButton = document.querySelector('#add-project-btn')
+    const newProjectForm = document.querySelector('.project-form')
+    const cancelProjectButton = document.querySelector('#cancel-project-btn')
+
+
+    newProjectButton.addEventListener('click',()=>{
+        newProjectForm.style.display = 'block';
+        newProjectButton.style.display = 'none';
+    })
+
+    cancelProjectButton.addEventListener('click',()=>{
+        newProjectForm.style.display = 'none';
+        newProjectButton.style.display = 'flex';
+    })
+
+    //saving project n appending new project to DOM
+    let projectTitle;
+    const projectInput = document.querySelector('#project-title')
+    const saveProjectButton = document.querySelector('#save-project-btn')
+    const sidebar = document.querySelector('.sidebar')
+
+    saveProjectButton.addEventListener('click',()=>{
+        if(projectInput.value.trim().length!=0)
+        {
+            projectTitle = projectInput.value.trim()
+            if(!(projectHandler.isDuplicate(projectTitle)))
+            {
+                let newProject = projectHandler.projectConstructor(projectTitle)
+                projectHandler.pushProject(newProject)
+                projectInput.value = ""
+                newProjectForm.style.display = 'none';
+                newProjectButton.style.display = 'flex';
+                let elt = document.createElement('div')
+                let temp = document.createElement('div')
+                elt.classList.add('project-item')
+                elt.setAttribute('data-title',projectTitle)
+                elt.setAttribute('tabindex','1')
+                temp.textContent = projectTitle
+                let icon = document.createElement('img')
+                icon.src = projectIcon;
+                elt.append(icon,temp)
+                sidebar.appendChild(elt)
+
+            }
+            else{
+                alert('project title already exists')
+                projectInput.value = ""
+            }
+        }
+        else{
+            alert("project title can't be empty")
+
+        }
+    })
+
+
+    //saving task
+
+    
+
+})();
+
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/images/'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+    
+    ],
+  },
+
+  devtool: 'inline-source-map',
+
+};
