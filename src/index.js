73ba93500a60fa7ae6000a47b8b20690be63087c
shahad7 <shahadneeliyatt786@gmail.com>
2023-06-12 22:50:45 +0530

    task edit,delete features added

--- src/index.js
+++ src/index.js
@@ -26,9 +26,9 @@ const taskHandler = (function (){
         {
             for(let j in projects[i].tasks)
             {
-                if(task==projects[i].tasks[j])
+                if(task==projects[i].tasks[j].name)
                 {
-                    found = 1;
+                    found = 1; 
                     return true
                 }
             }
@@ -53,7 +53,54 @@ const taskHandler = (function (){
        
     }
 
-    return {taskConstructor,isDuplicate,pushTask}
+    const updateTask = (obj,task,project) =>{
+        let projects = projectHandler.getProjects()
+        let projectIndex,taskIndex;
+        for(let i in projects)
+        {
+            if(projects[i].title==project)
+            {
+                projectIndex = i;
+                break;
+            }
+        }
+    
+        for(let j in projects[projectIndex].tasks)
+        {
+            if(projects[projectIndex].tasks[j].name==task)
+            {
+                taskIndex = j
+                break;
+            }
+        } 
+        projects[projectIndex].tasks[taskIndex] = obj;
+    }
+
+    const cleanUp = (project,task)=>{
+        let projects = projectHandler.getProjects()
+        let projectIndex,taskIndex;
+        for(let i in projects)
+        {
+            if(projects[i].title==project)
+            {
+                projectIndex = i;
+                break;
+            }
+        }
+    
+        for(let j in projects[projectIndex].tasks)
+        {
+            if(projects[projectIndex].tasks[j].name==task)
+            {
+                taskIndex = j
+                break;
+            }
+        }
+        projects[projectIndex].tasks.splice(taskIndex,1)
+
+    }
+
+    return {taskConstructor,isDuplicate,pushTask,updateTask,cleanUp}
 })();
 
 
@@ -96,7 +143,7 @@ const projectHandler = (function (){
         projects = projects.filter((project)=>{
             return project.title!=str
         })
-        console.log(projects)
+        
     }
 
     return{projectConstructor,pushProject,getProjects,isDuplicate,deleteProject}
@@ -106,22 +153,21 @@ const projectHandler = (function (){
 
 const DOMHandler = (function (){
 
-    //event listeners
 
     //to change input range color based on chosen priority
     const range = document.getElementById('priority')
     range.value = '1';
     range.addEventListener('input',()=>{
-        changeColor()
+        changeColor(range,range.value)
     })
 
-    function changeColor(){
-        if(range.value=='0')
-            range.style.accentColor = 'green'
-        else if(range.value=='1')
-            range.style.accentColor = 'yellow'
-        else if(range.value=='2')
-            range.style.accentColor = 'red'
+    function changeColor(elt,value){
+        if(value=='0')
+           elt.style.accentColor = 'green'
+        else if(value=='1')
+            elt.style.accentColor = 'yellow'
+        else if(value=='2')
+            elt.style.accentColor = 'red'
     }
 
 
@@ -130,11 +176,20 @@ const DOMHandler = (function (){
     const newTaskButton = document.querySelector('.add-task')
     const newTaskForm = document.querySelector('.add-task-form')
     const content = document.querySelector('.content')
+    const taskName = document.querySelector('#task-name')
+    const taskDesc = document.querySelector('#task-desc')
+    const taskDue = document.querySelector('#date')
+    const taskPriority = document.querySelector('#priority')
 
     formCloseButton.addEventListener('click',()=>{
         newTaskForm.style.visibility = 'hidden'
         newTaskForm.style.transform = 'scale(0.2)'
         content.style.filter = 'none'
+        taskDesc.value = ""
+        taskName.value = ""
+        taskDue.value = null
+        taskPriority.value = '1'
+        taskPriority.style.accentColor = 'yellow'
     })
 
     newTaskButton.addEventListener('click',()=>{
@@ -142,6 +197,61 @@ const DOMHandler = (function (){
         newTaskForm.style.transform = 'scale(1)'
         content.style.filter = 'blur(8px)'
     })
+
+
+    //closing edit-task-from
+    const editFormCloseButton = document.querySelector('#edit-close-button')
+    const saveEditButton = document.querySelector('#edit-button')
+    const editTaskForm = document.querySelector('.edit-task-form')
+    const editTaskName = document.querySelector('#edit-task-name')
+    const editTaskDesc = document.querySelector('#edit-task-desc')
+    const editTaskDue = document.querySelector('#edit-date')
+    const editTaskPriority = document.querySelector('#edit-priority')
+    editTaskPriority.addEventListener('input',()=>{
+        changeColor(editTaskPriority,editTaskPriority.value)
+    })
+
+    editFormCloseButton.addEventListener('click',()=>{
+        editTaskForm.style.visibility = 'hidden'
+        editTaskForm.style.transform = 'scale(0.2)'
+        content.style.filter = 'none'
+        editTaskDesc.value = ""
+        editTaskName.value = ""
+        editTaskDue.value = null
+        editTaskPriority.value = '1'
+        editTaskPriority.style.accentColor = 'yellow'
+    })
+
+
+    //saving edits to task
+    saveEditButton.addEventListener('click',()=>{
+        if(!taskHandler.isDuplicate(editTaskName.value))
+        {
+            if(editTaskName.value.trim().length!=0&&editTaskDesc.value.trim().length!=0&&
+                editTaskDue.value.trim().length!=0&&editTaskPriority.value.trim().length!=0)
+            {
+                let modifiedTask = taskHandler.taskConstructor(editTaskName.value,editTaskDesc.value,
+                    editTaskDue.value,editTaskPriority.value)
+                taskHandler.updateTask(modifiedTask,currentTask,currentProject)
+                addTasksToList()
+                editTaskForm.style.visibility = 'hidden'
+                editTaskForm.style.transform = 'scale(0.2)'
+                content.style.filter = 'none'
+                editTaskDesc.value = ""
+                editTaskName.value = ""
+                editTaskDue.value = null
+                editTaskPriority.value = '1'
+                editTaskPriority.style.accentColor = 'yellow'
+            }
+            else{
+                alert('fields can\'t be empty')
+            }
+        }
+        else{
+            alert('task name must be unique')
+        }
+    })
+    
 
 
     //new project opening n closing
@@ -183,6 +293,11 @@ const DOMHandler = (function (){
 
                 // event listener for non-default projects to add their tasks to DOM
                 elt.addEventListener('click',()=>{
+                    if(currentProject!="")
+                    {
+                        document.querySelector(`[data-title=${currentProject}]`).style.backgroundColor = 'initial'
+                    }
+                    elt.style.backgroundColor = 'rgba(255, 255, 255, .1)'
                     taskList.replaceChildren()
                     currentProject = elt.getAttribute('data-title')
                     addTasksToList()
@@ -212,7 +327,7 @@ const DOMHandler = (function (){
     })
 
     //viewing non-default projects
-
+    const taskList = document.querySelector('.list')
     const addTasksToList = ()=>{
         let projects = projectHandler.getProjects()
         let index;
@@ -233,9 +348,8 @@ const DOMHandler = (function (){
                 items.push(constructTaskElement(tasks[j].name,tasks[j].description,
                     tasks[j].dueDate,tasks[j].priority))
             }
-            taskList.replaceChildren(items)
-            console.log(items)
-            //start from here next time
+            taskList.replaceChildren(...items)
+        
         }
     }
 
@@ -247,16 +361,26 @@ const DOMHandler = (function (){
 
     [inbox,today,week].forEach(elt=>{
 
+    
         if(elt==inbox)
         {
             elt.addEventListener('click',()=>{
+                if(currentProject!="")
+                    document.querySelector(`[data-title=${currentProject}]`).style.backgroundColor = 'initial'
+                elt.style.backgroundColor = 'rgba(255, 255, 255, .1)'
+                taskList.replaceChildren()
                 currentProject = elt.getAttribute('data-title')
+                addTasksToList()
                 deleteProjectButton.style.display = 'none'
                 newTaskButton.style.display = 'flex'
             })
         }
         else{
             elt.addEventListener('click',()=>{
+                elt.style.backgroundColor = 'rgba(255, 255, 255, .1)'
+                if(currentProject!="")
+                    document.querySelector(`[data-title=${currentProject}]`).style.backgroundColor = 'initial'
+                taskList.replaceChildren()
                 currentProject = elt.getAttribute('data-title')
                 deleteProjectButton.style.display = 'none'
                 newTaskButton.style.display = 'none'
@@ -264,6 +388,11 @@ const DOMHandler = (function (){
         }
      }
     )
+
+    //on page loading inbox must be automatically selected
+    window.addEventListener('load',()=>{
+        inbox.click()   
+    })
 
     //deleting projects
     const deleteProjectButton = document.querySelector('#delete-project-btn')
@@ -274,57 +403,87 @@ const DOMHandler = (function (){
             taskList.replaceChildren()
             projectHandler.deleteProject(currentProject)
             currentProject = ""
+            newTaskButton.style.display = 'none'
+            deleteProjectButton.style.display = 'none'
             }
     })
 
 
+    let currentTask;
     //function to create a task DOM element
+
     function constructTaskElement(nameArg,descArg,dueDateArg,priorityArg){
-     
-    let task = document.createElement('div')
-    task.classList.add('item')
-    let name = document.createElement('div')
-    name.textContent = nameArg
-    name.setAttribute('id','task-DOM-name')
-    let flag = document.createElement('img')
-    flag.setAttribute('id','flag')
-    flag.src = flagIcon
-    console.log(nameArg+priorityArg)
-    if(priorityArg=='0')
-        flag.style.filter = 'invert(30%) sepia(83%) saturate(402%) hue-rotate(31deg) brightness(95%) contrast(86%)'
-    else if(priorityArg=='2')
-        flag.style.filter = 'invert(91%) sepia(148%) saturate(4729%) hue-rotate(359deg) brightness(137%) contrast(175%)'
-    else
-        flag.style.filter = 'invert(21%) sepia(88%) saturate(6850%) hue-rotate(357deg) brightness(96%) contrast(112%)'        
 
-    let dueDate = document.createElement('div')
-    dueDate.textContent = dueDateArg
-    let options = document.createElement('div')
-    options.classList.add('options')
-    let eye = document.createElement('img')
-    eye.setAttribute('id','eye')
-    eye.src = eyeIcon
-    let pencil = document.createElement('img')
-    pencil.setAttribute('id','pencil')
-    pencil.src = pencilIcon
-    let trash = document.createElement('img')
-    trash.setAttribute('id','trash')
-    trash.src = trashIcon
-    options.append(eye,pencil,trash)
-    task.append(name,flag,dueDate,options)
+        let task = document.createElement('div')
+        task.classList.add('item')
+        task.setAttribute('data-title',nameArg)
+        let name = document.createElement('div')
+        name.textContent = nameArg
+        name.setAttribute('id','task-DOM-name')
+        let flag = document.createElement('img')
+        flag.setAttribute('id','flag')
+        flag.src = flagIcon
 
-    return task;
+        if(priorityArg=='0')
+            flag.style.filter = 'invert(30%) sepia(83%) saturate(402%) hue-rotate(31deg) brightness(95%) contrast(86%)'
+        else if(priorityArg=='2')
+            flag.style.filter = 'invert(91%) sepia(148%) saturate(4729%) hue-rotate(359deg) brightness(137%) contrast(175%)'
+        else
+            flag.style.filter = 'invert(21%) sepia(88%) saturate(6850%) hue-rotate(357deg) brightness(96%) contrast(112%)'        
+
+        let dueDate = document.createElement('div')
+        dueDate.textContent = dueDateArg
+        let options = document.createElement('div')
+        options.classList.add('options')
+        let eye = document.createElement('img')
+        eye.setAttribute('id','eye')
+        eye.src = eyeIcon
+        let pencil = document.createElement('img')
+        pencil.setAttribute('id','pencil')
+        pencil.src = pencilIcon
+
+        //to view edit-task-form
+        pencil.addEventListener('click',()=>{
+            editTaskForm.style.visibility = 'visible'
+            editTaskName.value = nameArg
+            editTaskDesc.value = descArg
+            editTaskDue.value = dueDateArg
+            editTaskPriority.value = priorityArg
+
+            currentTask = nameArg
+
+            if(editTaskPriority.value=='0')
+            editTaskPriority.style.accentColor = 'green'
+            else if(editTaskPriority.value=='1')
+            editTaskPriority.style.accentColor = 'yellow'
+            else if(editTaskPriority.value=='2')
+            editTaskPriority.style.accentColor = 'red'
+
+            editTaskForm.style.transform = 'scale(1)'
+            content.style.filter = 'blur(8px)'
+        })
+
+        let trash = document.createElement('img')
+        trash.setAttribute('id','trash')
+        trash.src = trashIcon
+
+        //deleting task
+        trash.addEventListener('click',()=>{
+            taskList.removeChild(task)
+            taskHandler.cleanUp(currentProject,nameArg)      
+        })
+
+
+        options.append(eye,pencil,trash)
+        task.append(name,flag,dueDate,options)
+
+        return task;
 
     }
     
 
     //saving task
     const saveTaskButton = document.querySelector('#save-button')
-    const taskName = document.querySelector('#task-name')
-    const taskDesc = document.querySelector('#task-desc')
-    const taskDue = document.querySelector('#date')
-    const taskPriority = document.querySelector('#priority')
-    const taskList = document.querySelector('.list')
 
     saveTaskButton.addEventListener('click',()=>{
 
